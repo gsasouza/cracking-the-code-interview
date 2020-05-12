b6e96c5facacf32639b5fc0af1e0f27f1f69ffb1
Gabriel Souza <gabrie.ssouza@gmail.com>
2020-05-12 20:54:41 -0300

    feat(string): add solution 7 in typescript

---
+++ array_strings/ts/7.ts
@@ -0,0 +1,31 @@
+const matrixRotate = (image: number[][]) => {
+  const size = image.length;
+  for (let i = 0; i < Math.floor(size/2); i++) {
+    for (let j = i; j < size - i - 1; j++) {
+      const aux = image[i][j];
+      image[i][j] = image[j][size - i - 1];
+      image[j][size - i - 1] = image[size - i - 1][size - j - 1];
+      image[size - i - 1][size - j - 1] = image[size - j - 1][i];
+      image[size - j - 1][i] = aux;
+
+    }
+  }
+  return image;
+}
+
+const image = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+const input = [...image];
+
+const expected = [
+  [3, 6, 9],
+  [2, 5, 8],
+  [1, 4, 7]
+];
+
+console.log('input: ', input, 'expected: ', expected)
+console.log('result: ', matrixRotate(image))
